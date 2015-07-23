b5e5ddad52ff2157b1625bc92634be38f5371282
chris <christoph@roboterclub-freiburg.de>
2015-07-23 10:36:47 +0200

    added title, xlable,ylabel

--- SVG_graphServer/SVG_graphServer.ino
+++ SVG_graphServer/SVG_graphServer.ino
@@ -62,9 +62,61 @@ void setup() {
 
 EthernetClient client;
 graph_t Graph1;
+graph_t Graph2;
 
 #define GRAPH1_LENGTH 100
 int16_t Graph1_data[GRAPH1_LENGTH];
+
+#define GRAPH2_LENGTH 100
+int16_t Graph2_data[GRAPH2_LENGTH];
+
+void printPar(char * str, int value)
+{
+    client.print(str);
+    client.print("=\"");
+    client.print(value);
+    client.print("\" ");
+}
+
+void rect(int x,int y,int width, int height)
+{
+  //<rect x="10" y="10" width="100" height="100"/>
+    client.print("<rect ");
+    printPar("x",x);
+    printPar("y",y);
+    printPar("width",width);
+    printPar("height",height);
+    client.print("style=\"fill:ghostwhite;stroke:black;stroke-width:1;stroke-opacity:0.9\" ");
+    client.print("/>");
+    // <rect x="50" y="20" width="150" height="150"
+    //style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9" />
+}
+
+void text(int x, int y, char *str, int rotation)
+{
+    //<text x="0" y="15" fill="red">I love SVG!</text>
+    client.print("<text ");
+    client.print("text-anchor=\"middle\"");
+    printPar("x",x);
+    printPar("y",y);
+    
+    // rotation
+    client.print("transform=\"rotate(");
+    client.print(rotation);
+    client.print(x);
+    client.print(",");
+    client.print(y);
+    client.print("\"");
+    
+    client.print("style=\"font-size:12px\"");
+    client.print(">");
+    client.print(str);
+    client.print("</text>");
+    //<text x="130" y="40" dx="0 0 0 -160" dy="0 0 0 -15" style="font-size:18px">
+    //<text x="0" y="15" fill="red" transform="rotate(30 20,40)">I love SVG</text>
+}
+
+#define GRAPHBORDER 30
 
 void graph(graph_t * g)
 {
@@ -74,36 +126,77 @@ void graph(graph_t * g)
 
   client.println("<svg xmlns=\"http://www.w3.org/2000/svg\"");
   client.println("version=\"1.1\" baseProfile=\"full\"");
-  client.println("width=\"700px\" height=\"400px\" viewBox=\"0 0 700 400\">");
+  //client.println("width=\"700px\" height=\"400px\" viewBox=\"0 0 700 400\">");
+  client.print("width=\"");client.print(g->width+2*GRAPHBORDER);client.print("px\"");
+  client.print("height=\"");client.print(g->height+2*GRAPHBORDER);client.print("px\"");
+  
+  //client.println("viewBox=\"0 0 700 400\" style="background: red" >");
+  client.print("viewBox=\"");
+  client.print(0);
+  client.print(0);
+  client.print(g->width);
+  client.print(g->height);
+  client.print("\" "); 
+  client.print("style=\"background: "); 
+  client.print("lightgrey");
+  client.print("\" "); 
+  client.println("\">");
 
+  // graph plot area background 
+  rect(GRAPHBORDER,GRAPHBORDER,g->width,g->height);
+  
   //********* start of the line plot ****************
   client.println("<polyline points=\"");
-  for (n = 0; n < GRAPH1_LENGTH ; ++n)
+  for (n = 0; n < g->data_length ; ++n)
   {
-    sprintf(strBuffer, "%d,%d ", n, g->data[n]);
+    sprintf(strBuffer, "%d,%d ", n+GRAPHBORDER, g->data[n]+GRAPHBORDER);
     client.print(strBuffer);
   }
   client.print("\" ");
 
-  client.print("stroke=\"green\" fill=\"none\" stroke-width=\"1px\"/>");
+  client.print("stroke=\"midnightblue\" fill=\"none\" stroke-width=\"1px\"/>");
   //*******  end of the line plot **********************************************
 
+  // print title
+  text(g->width/2+GRAPHBORDER,GRAPHBORDER/2,g->title,0);
+  
+  // print xlabel
+  text(g->width/2+GRAPHBORDER,g->height + GRAPHBORDER + GRAPHBORDER/2 ,g->xlabel,20);  
+  
   client.println("</svg>");
 }
 
 
-
 void loop()
 {
-  char strBuffer[100];
+  char strBuffer[40];
   int n;
   
+  Graph1.title       = "Analog Channel 0";
+  Graph1.xlabel      = "[n]";
+  Graph1.ylabel      = "[v]";
+  Graph1.data        = Graph1_data;
+  Graph1.data_length = GRAPH1_LENGTH;
+  Graph1.width       = 200;
+  Graph1.height      = 150;
+
   for (n = 0; n < GRAPH1_LENGTH ; ++n)
   {
     Graph1_data[n]= analogRead(A0);
   }
   
-  Graph1.data=Graph1_data;
+  Graph2.title       = "Analog Channel 1";
+  Graph2.xlabel      = "[n]";
+  Graph2.ylabel      = "[v]";
+  Graph2.data        = Graph2_data;
+  Graph2.data_length = GRAPH2_LENGTH;
+  Graph2.width       = 300;
+  Graph2.height      = 200;
+
+  for (n = 0; n < GRAPH2_LENGTH ; ++n)
+  {
+    Graph2_data[n]= analogRead(A1);
+  }
   
   // listen for incoming clients
   client = server.available();
@@ -138,26 +231,9 @@ void loop()
           client.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
           //******** start the SVG grafics ********************************************
-
-          client.println("<svg xmlns=\"http://www.w3.org/2000/svg\"");
-          client.println("version=\"1.1\" baseProfile=\"full\"");
-          client.println("width=\"700px\" height=\"400px\" viewBox=\"0 0 700 400\">");
-
-          //********* start of the line plot ****************
-          client.println("<polyline points=\"");
-          for (n = 0; n < 700 ; ++n)
-          {
-            sprintf(strBuffer, "%d,%d ", n, analogRead(A0));
-            client.print(strBuffer);
-          }
-          client.print("\" ");
-
-          client.print("stroke=\"green\" fill=\"none\" stroke-width=\"1px\"/>");
-          //*******  end of the line plot **********************************************
-
-          client.println("</svg>");
           
           graph(&Graph1);
+          graph(&Graph2);
 
           break;
         }
--- SVG_graphServer/graph.h
+++ SVG_graphServer/graph.h
@@ -5,5 +5,10 @@
 typedef struct
 {
   int16_t *data;
-  uint16_t data_length;  
+  uint16_t data_length;
+  uint16_t width;
+  uint16_t height;
+  char * title;
+  char * xlabel;
+  char * ylabel;  
 } graph_t;
